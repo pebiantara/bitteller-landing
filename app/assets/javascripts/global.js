0f90cb428e869bc5240f725e23776210b597badb
pebiantara <pebiantara@gmail.com>
2016-02-22 20:54:51 +0700

    improvement style

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -2,5 +2,8 @@ $(document).on('ready page:load', function(){
 	$(".title-accordion").click(function(){
     element = $(this).data('target');
     $("#"+element).slideToggle();
+    $(window).resize(function(){
+    	alert($(window).width());
+    })
 	})
 });
